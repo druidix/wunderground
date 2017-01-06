d7e68ceeb0495817d33b91dbd1eb7c7ac1096e33
Kaushik Acharya <kaushik@palebluedot.net>
2017-01-06 13:10:55 -0800

    Finally got the hash lookup working Added function to make int out of supplied zip code string.  Removed most of the debug.  Next up: -  Make query URL dynamic using supplied arg

--- src/wunderground/core.clj
+++ src/wunderground/core.clj
@@ -4,28 +4,26 @@
             [clojure.data.json :as json])
   )
 
+(defn parse-int [s]
+  (Integer. (re-find #"\d+" s)))
+
 (defn -main
   [& args]
-  (println "Args were: " args)
   (def api-key "367af81f6daa0a7b")
   (def api-base-url "http://api.wunderground.com/api/")
   (def weather-query "/conditions/q/CA/94558.json")
   (def query-url (str api-base-url api-key weather-query))
-  (prn 'URL  query-url)
-                                        ;(def response (client/get query-url))
-                                        ;(prn 'RESPONSE response)
+                                        ;(prn 'URL  query-url)
   (client/get query-url
           (fn [{:keys [status headers body error]}] ;; asynchronous response handling
             (if error
               (println "Failed, exception is " error)
               (do
-                (println "Async HTTP GET status: " status)
+                                        ;(println "Async HTTP GET status: " status)
                                         ;(println "Async HTTP GET headers: " headers)
                                         ;(println "Async HTTP GET body: " body)
                 (let [body-map (json/read-str body)]
                   (do
-                                        ;(prn 'FROM_HTTP body)
-                    (prn 'FROM_LIB body-map)
-                    (println "\n\n")
-                    (prn 'RESPONSE (get-in body-map [:response]))
-                    (println "\n\n"))))))))
+                    (println "Current temp in" (parse-int (str args)) ":  "
+                             (get-in body-map ["current_observation" "temp_c"]) "C\n"
+                             "Station ID:  " (get-in body-map ["current_observation" "station_id"]) "\n"))))))))
